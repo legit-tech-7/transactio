2a76b8c5b397dda99e33795d86ba9b6e9e5b6ad0
joshua <joshuachukwuemeka52@gmail.com>
2026-04-08 17:47:14 +0100

    Updated project

--- tras/settings.py
+++ tras/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!ge-%ug(unokh2y@_q+702j$$n*mp=!afp7i5rmmp!*%ut1y$w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
